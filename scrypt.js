28c31a5a20dc451f5b9441235aaf04d41419fab7
Arefev <127677998+hlebushekl@users.noreply.github.com>
2024-01-24 14:22:19 +0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,24 @@
+const alt = "qwerty123";
+const klown = 778779;
+const block = false;
+
+x1 = String(alt);
+x2 = Number(alt);
+x3 = Boolean(alt);
+console.log(x1);
+console.log(x2);
+console.log(x3);
+
+y1 = String(klown);
+y2 = Number(klown);
+y3 = Boolean(klown);
+console.log(y1);
+console.log(y2);
+console.log(y3);
+
+z1 = String(block);
+z2 = Number(block);
+z3 = Boolean(block);
+console.log(z1);
+console.log(z2);
+console.log(z3);
